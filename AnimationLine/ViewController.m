bda7bffbb0a4f3690cc8a871d65d2c1c4961af47
YouXianMing <705786299@qq.com>
2015-07-04 13:56:30 +0800

    1.0

--- AnimationLine/ViewController.m
+++ AnimationLine/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LineAnimationView.h"
 
 @interface ViewController ()
 
@@ -16,12 +17,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIView *redView = [[UIView alloc] initWithFrame:CGRectMake(100 - 5, 100, 3, 20 / 2)];
+    redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:redView];
+    
+    LineAnimationView *lineAnimationView1 = [[LineAnimationView alloc] initWithFrame:CGRectMake(100, 100, 200 / 2, 20 / 2)];
+    lineAnimationView1.sourceImage        = [UIImage imageNamed:@"demo"];
+    lineAnimationView1.animationType      = RIGHT_TO_LEFT;
+    [self.view addSubview:lineAnimationView1];
+    [lineAnimationView1 startAnimation];
+    
+    LineAnimationView *lineAnimationView2 = [[LineAnimationView alloc] initWithFrame:CGRectMake(100, 140, 720 / 6.f, 18 / 6.f)];
+    lineAnimationView2.sourceImage        = [UIImage imageNamed:@"demo2"];
+    lineAnimationView2.animationType      = LEFT_TO_RIGHT;
+    lineAnimationView2.duration           = 3.f;
+    lineAnimationView2.alpha              = 0.3f;
+    [self.view addSubview:lineAnimationView2];
+    [lineAnimationView2 startAnimation];
 }
 
 @end
